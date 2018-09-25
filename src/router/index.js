e5746b7e83d69010acc696892bf9e0d319ec8789
TangJunChao <junchaotang@qq.com>
2018-09-25 16:21:00 +0800

    登录界面搭建

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue';
 import Router from 'vue-router';
 import Home from '@/components/home/home';
 import UserList from '@/components/userlist/user-list';
+import Login from '@/views/login';
 
 Vue.use(Router);
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: Login
+    },
     {
       path: '/',
       name: 'home',
---
+++ src/views/login.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="login">
+    <el-form class="login-form" label-position="top" ref="form" :model="form" label-width="80px">
+      <h2>用户登录</h2>
+      <el-form-item label="用户名">
+        <el-input v-model="form.username"></el-input>
+      </el-form-item>
+      <el-form-item label="密码">
+        <el-input v-model="form.password"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button class="login-btn" type="primary">登 录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      form: {
+        username: '',
+        password: ''
+      }
+    };
+  }
+};
+</script>
+
+<style>
+.login{display:flex;justify-content: center;align-items: center;height:100%;background:#324152;}
+.login .login-form{width:400px;padding:30px;background:#fff;border-radius:5px;}
+.login .login-form .login-btn{width:100%;}
+</style>
